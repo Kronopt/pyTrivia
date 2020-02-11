555a825e681bf3b6920a6dcf6a57a0e837b8b10e
Kronopt <Kronopt@users.noreply.github.com>
2020-02-11 08:44:43 +0000

    pyTrivia.Client
    - Changed self.categories to self._categories_and_ids
    - Finished __init__
    - Added categories, difficulties and types properties
    - Added get_questions method
    - Added response_code parameter to _request_resource and new logic to allow ignoring the json response_code flag (for OpenTriviaDB API calls where it doesn't exist, like the helper OpenTriviaDB API functions)

--- pytrivia/client.py
+++ pytrivia/client.py
@@ -34,8 +34,31 @@ class Client:
         HTTP header sent with every request.
     token : str
         Token in use.
-    categories : dict
-        Categories and respective ids.
+    categories : tuple
+        All available question categories.
+    difficulties : tuple
+        All available question difficulties.
+    types : tuple
+        All available question types.
+
+    Methods
+    -------
+    get_questions(number=1, category=None, difficulty=None, _type=None)
+        Retrieves questions from the OpenTriviaDB API.
+
+    Raises
+    ------
+    TypeError
+        When calling get_questions, if number is not and int between 1 and 50.
+        When calling get_questions, if category is not a str in self.categories.
+        When calling get_questions, if difficulty is not a str in self.difficulties.
+        When calling get_questions, if _type is not a str in self.types.
+    ValueError
+        When calling __init__, get_questions, if the HTTP response body does not contain valid json (request.json error).
+    HttpError
+        When calling __init__, get_questions, if the HTTP code is not 200 OK.
+    NoResults, InvalidParameter, TokenNotFound, TokenEmpty, UnexpectedResponseCode
+        When calling __init__, get_questions, if the OpenTriviaDB response code is not 0.
     """
 
     MAIN_URL = 'https://opentdb.com/'
@@ -55,7 +78,7 @@ class Client:
         Initializes the pyTrivia client.
         Performs 2 requests to the OpenTriviaDB API:
             - Token request
-            - Categories request
+            - Available categories request
 
         Raises
         ------
@@ -67,7 +90,105 @@ class Client:
             If the OpenTriviaDB response code is not 0.
         """
         self.token = self._get_token()
-        self.categories = self._get_categories()
+        self._categories_and_ids = self._get_categories()
+        self._difficulties = ('easy', 'medium', 'hard')
+        self._types = ('multiple', 'boolean')
+
+    @property
+    def categories(self):
+        """
+        Shows all available categories.
+
+        Returns
+        -------
+        tuple
+            Tuple of all valid categories.
+        """
+        return tuple(self._categories_and_ids)  # keys only, which are the category names
+
+    @property
+    def difficulties(self):
+        """Returns _difficulties. All available difficulties."""
+        return self._difficulties
+
+    @property
+    def types(self):
+        """Returns _types. All available types."""
+        return self._types
+
+    def get_questions(self, number=1, category=None, difficulty=None, _type=None):
+        """
+        Retrieves questions from OpenTriviaDB.
+
+        Parameters
+        ----------
+        number : int, optional
+            Number of questions to retrieve (0 < number <= 50).
+        category : str or None, optional
+            Question category. One of self.categories.
+        difficulty : str or None, optional
+            Question difficulty. One of self._difficulties
+        _type : str or None, optional
+            Type of question. One of self._types
+
+        Returns
+        -------
+        list(Question)
+
+        Raises
+        ------
+        TypeError
+            If number is not and int between 1 and 50.
+            If category is not a str in self.categories.
+            If difficulty is not a str in self._difficulties.
+            If _type is not a str in self._types.
+        ValueError
+            If the HTTP response body does not contain valid json (request.json error).
+        HttpError
+            If the HTTP code is not 200 OK.
+        NoResults, InvalidParameter, TokenNotFound, TokenEmpty, UnexpectedResponseCode
+            If the OpenTriviaDB response code is not 0.
+        """
+        # number
+        if not (isinstance(number, int) and (0 < number <= 50)):
+            raise TypeError('Parameter \'number\' must be an int between 1 and 50')
+        # category
+        if category is None:
+            category = ''
+        elif category in self._categories_and_ids:
+            category = '&category=%i' % self._categories_and_ids[category]
+        else:
+            raise TypeError('Parameter \'category\' must be an existing category. '
+                            'Call Client.category to check available categories.')
+        # difficulty
+        if difficulty is None:
+            difficulty = ''
+        elif difficulty in self._difficulties:
+            difficulty = '&difficulty=%s' % difficulty
+        else:
+            raise TypeError('Parameter \'difficulty\' must be one of: %s' % ', '.join(self._difficulties))
+        # _type
+        if _type is None:
+            _type = ''
+        elif _type in self._types:
+            _type = '&type=%s' % _type
+        else:
+            raise TypeError('Parameter \'_type\' must be one of: %s' % ', '.join(self._types))
+
+        api_url = '%s?amount=%i%s%s%s&token=' % (self.API_URL, number, category, difficulty, _type)
+
+        # handle token
+        try:
+            json = self._request_resource(api_url + self.token)
+        except TokenNotFound:  # get a new token
+            self.token = self._get_token()
+            # if this throws another TokenNotFound, something is up with the API...
+            json = self._request_resource(api_url + self.token)
+        except TokenEmpty:  # reset token
+            self.token = self._get_token(reset=True)
+            # if this throws another TokenEmpty, something is up with the API...
+            json = self._request_resource(api_url + self.token)
+        return [Question(**question) for question in json['results']]
 
     def _get_token(self, reset=False):
         """
@@ -126,14 +247,14 @@ class Client:
         NoResults, InvalidParameter, TokenNotFound, TokenEmpty, UnexpectedResponseCode
             If the OpenTriviaDB response code is not 0.
         """
-        json = self._request_resource(self.ALL_CATEGORIES_URL)
+        json = self._request_resource(self.ALL_CATEGORIES_URL, response_code=False)
         # Assumes json is structured as per the OpenTriviaDB documentation.
         # Any error that arises from "wrong" dictionary keys is because json response was
         # altered by OpenTriviaDB in the meanwhile.
         categories = {category['name']: category['id'] for category in json['trivia_categories']}
         return categories
 
-    def _request_resource(self, url):
+    def _request_resource(self, url, response_code=True):
         """
         Performs an HTTP request to an OpenTriviaDB url and retrieves the json response.
 
@@ -141,6 +262,8 @@ class Client:
         ----------
         url : str
             The HTTP request will be made to this url.
+        response_code : bool
+            Whether to check for response_code or not
 
         Returns
         -------
@@ -160,18 +283,19 @@ class Client:
         if response.status_code != 200:
             raise HttpError(response.status_code, response.reason)
         json = response.json()
-        # Assumes json is structured as per the OpenTriviaDB documentation.
-        # Any error that arises from "wrong" dictionary keys is because json response was
-        # altered by OpenTriviaDB in the meanwhile.
-        response_code = json['response_code']
-        if response_code == 1:
-            raise NoResults()
-        elif response_code == 2:
-            raise InvalidParameter()
-        elif response_code == 3:
-            raise TokenNotFound()
-        elif response_code == 4:
-            raise TokenEmpty()
-        elif response_code != 0:
-            raise UnexpectedResponseCode(str(response_code))
+        if response_code:
+            # Assumes json is structured as per the OpenTriviaDB documentation.
+            # Any error that arises from "wrong" dictionary keys is because json response was
+            # altered by OpenTriviaDB in the meanwhile.
+            response_code = json['response_code']
+            if response_code == 1:
+                raise NoResults()
+            elif response_code == 2:
+                raise InvalidParameter()
+            elif response_code == 3:
+                raise TokenNotFound()
+            elif response_code == 4:
+                raise TokenEmpty()
+            elif response_code != 0:
+                raise UnexpectedResponseCode(str(response_code))
         return json
